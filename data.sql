ca89d87cf8e68cda2b84630d648f37e9f5b23e3a
Ismail <ismailmunyentwari9@gmail.com>
2023-05-03 18:12:02 +0300

    Add constraints, species table and owners table

--- data.sql
+++ data.sql
@@ -16,3 +16,40 @@ VALUES
 ('Blossom', '1998-10-13', 3, true, 17, NULL),
 ('Ditto', '2022-05-14', 4, true, 22, NULL);
 
+-- Add data to owners table
+INSERT into owners(full_name,age) 
+VALUES('Sam Smith', 34),
+      ('Jennifer Orwell', 19),
+      ('Bob', 45),
+      ('Melody Pond', 77), 
+      ('Dean Winchester',14),
+      ('Jodie Whittaker', 39);
+
+-- Add data to owners table
+INSERT into species(name)
+VALUES('Pokemon'),
+      ('Digimon');
+
+-- Add species_id
+UPDATE animals
+SET species_id = CASE
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+  END;
+
+  -- update animals with owner_id based on owner name
+UPDATE animals
+SET owner_id = CASE
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  END ;
+
+
+
+
+
+
+                                   
